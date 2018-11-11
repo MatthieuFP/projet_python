0613d6a1979073799c01eb78e9a92f654b3dd0c7
LouisChess <louis.pruvot_caprioli@edu.escpeurope.eu>
2018-11-11 15:48:45 +0100

    creating methods and deleting outliers

---
+++ README.md
@@ -0,0 +1,2 @@
+# projet_python
+Predicting taxi fares in New York City
---
+++ support/constants.py
@@ -0,0 +1,24 @@
+# PATHS
+Root_data = "data"
+Path_train = Root_data + r"\train.csv"
+Path_test = Root_data + r"\test.csv"
+Root_graphs = "graphs"
+
+# DF COLUMNS
+# Original Columns
+Key = 'key'
+Fare_amount= 'fare_amount'
+Pickup_datetime = 'pickup_datetime'
+Pickup_longitude = 'pickup_longitude'
+Pickup_latitude = 'pickup_latitude'
+Dropoff_longitude = 'dropoff_longitude'
+Dropoff_latitude = 'dropoff_latitude'
+Passenger_count = 'passenger_count'
+
+# Added Columns
+Date = 'date'
+Time = 'time'
+Week_day = 'week_day'
+Distance = "distance"
+Outlier = 'outlier'
+
